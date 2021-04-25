56866234b3874c79b088e8a3643c2cf4c3451815
Anthony Alvarez <anscalva@ucsc.edu>
2021-04-25 06:11:06 -0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const tileSize = 35;
+
+let config = {
+    type: Phaser.WEBGL,
+    width: 840,
+    height: 525,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 1800
+            }
+        }
+    },
+    scene: [Menu, Play]
+}
+
+let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+let keySPACE, keyUP, keyDOWN, keyR;
